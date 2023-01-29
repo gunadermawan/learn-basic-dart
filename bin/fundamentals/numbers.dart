0f106de3e9003b877a95719357cea51773f1bec4
gunadermawan <gunadermawan.official@gmail.com>
2023-01-29 16:57:52 +0700

    fundamentals: number

---
+++ bin/fundamentals/numbers.dart
@@ -0,0 +1,23 @@
+// two type of number is int and double
+
+// int
+var age = 2;
+int weight = 20;
+
+// double
+var width = 12.2;
+double height = 145.3;
+
+// parse number
+
+//  string -> int
+var one = int.parse('1');
+
+// String -> double
+var onePointTwo = double.parse('1.2');
+
+// int -> string
+var oneAsString = 1.toString();
+
+// double -> string
+var phiAsString = 3.14159.toStringAsFixed(2);
